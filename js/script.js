94b78f791f53296e9ffff53b2c8fa8cf76ab8ce9
AidanMaldonado <aidanm1198@gmail.com>
2018-05-14 22:05:55 -0400

    Updated layout
    CSS and HTML layouts updated with buttons, flex and basic layout

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/*
+let div = document.querySelector('.header');
+let navCity = document.createElement('BUTTON');
+let buttonText = document.createTextNode('City');
+navCity.appendChild(buttonText);
+navCity.classList.add('');
+div.appendChild(navCity);
+*/
