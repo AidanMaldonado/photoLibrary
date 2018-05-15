0874634f900e295ba41cbabb5d9a884f38e99d44
AidanMaldonado <aidanm1198@gmail.com>
2018-05-14 23:10:15 -0400

    Updated Buttons
    Radio buttons are now normal buttons. Clicking each button displays pictures that are associated with it's tag

--- js/script.js
+++ js/script.js
@@ -1,8 +1,46 @@
+//Getting Id's for buttons from the dom
+let cityNav = document.querySelector('#city');
+let beachNav = document.getElementById('beach');
+let mountainNav = document.querySelector('#mountain');
+let city = document.querySelector('.city');
+let beach = document.getElementsByClassName('.beach');
+let mountain = document.querySelector('.mounntain');
+
+beachNav.addEventListener("click", clickTagBeach);
+
+function clickTagBeach () {
+  for(let i = 0; i < 3; i++) {
+    document.getElementsByClassName("city")[i].style.display = "none";
+    document.getElementsByClassName("mountain")[i].style.display = "none";
+    document.getElementsByClassName("beach")[i].style.display = "";
+  }
+}
+
+cityNav.addEventListener("click", clickTagCity);
+
+function clickTagCity () {
+  for(let i = 0; i < 3; i++) {
+    document.getElementsByClassName("city")[i].style.display = "";
+    document.getElementsByClassName("mountain")[i].style.display = "none";
+    document.getElementsByClassName("beach")[i].style.display = "none";
+  }
+}
+
+mountainNav.addEventListener("click", clickTagMountain);
+
+function clickTagMountain () {
+  for(let i = 0; i < 3; i++) {
+    document.getElementsByClassName("city")[i].style.display = "none";
+    document.getElementsByClassName("mountain")[i].style.display = "";
+    document.getElementsByClassName("beach")[i].style.display = "none";
+  }
+}
+
+
 /*
-let div = document.querySelector('.header');
-let navCity = document.createElement('BUTTON');
-let buttonText = document.createTextNode('City');
-navCity.appendChild(buttonText);
-navCity.classList.add('');
-div.appendChild(navCity);
+const clickEvent = () => {
+  beach.style.visibility = "hidden";
+};
+
+cityNav.append(clickEvent);
 */
