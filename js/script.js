3fce969c6ade6611dbe97459ffeeafc900cc6ce3
AidanMaldonado <aidanm1198@gmail.com>
2018-05-14 23:19:48 -0400

    Added button
    Added button that shows all images to reset

--- js/script.js
+++ js/script.js
@@ -1,12 +1,26 @@
 //Getting Id's for buttons from the dom
 let cityNav = document.querySelector('#city');
-let beachNav = document.getElementById('beach');
+let beachNav = document.querySelector('#beach');
 let mountainNav = document.querySelector('#mountain');
+let showNav = document.querySelector('#show');
 let city = document.querySelector('.city');
 let beach = document.getElementsByClassName('.beach');
 let mountain = document.querySelector('.mounntain');
 
+//Event listeners for each button
 beachNav.addEventListener("click", clickTagBeach);
+cityNav.addEventListener("click", clickTagCity);
+mountainNav.addEventListener("click", clickTagMountain);
+showNav.addEventListener("click", clickTagMain);
+
+//Functions that hide the respective tags for each button click
+function clickTagMain () {
+  for(let i = 0; i < 3; i++) {
+    document.getElementsByClassName("city")[i].style.display = "";
+    document.getElementsByClassName("mountain")[i].style.display = "";
+    document.getElementsByClassName("beach")[i].style.display = "";
+  }
+}
 
 function clickTagBeach () {
   for(let i = 0; i < 3; i++) {
@@ -16,8 +30,6 @@ function clickTagBeach () {
   }
 }
 
-cityNav.addEventListener("click", clickTagCity);
-
 function clickTagCity () {
   for(let i = 0; i < 3; i++) {
     document.getElementsByClassName("city")[i].style.display = "";
@@ -26,8 +38,6 @@ function clickTagCity () {
   }
 }
 
-mountainNav.addEventListener("click", clickTagMountain);
-
 function clickTagMountain () {
   for(let i = 0; i < 3; i++) {
     document.getElementsByClassName("city")[i].style.display = "none";
@@ -35,12 +45,3 @@ function clickTagMountain () {
     document.getElementsByClassName("beach")[i].style.display = "none";
   }
 }
-
-
-/*
-const clickEvent = () => {
-  beach.style.visibility = "hidden";
-};
-
-cityNav.append(clickEvent);
-*/
